abd65ec652aff082fe376fbbe6c3da4577cba507
hamnahyeon <hammcoder@gmail.com>
2021-08-02 17:08:06 +0900

    2021.08.02 커밋푸쉬

--- wooper/src/main/java/edu/fin/upa/calendar/model/service/CalendarService.java
+++ wooper/src/main/java/edu/fin/upa/calendar/model/service/CalendarService.java
@@ -1,5 +1,13 @@
-package edu.fin.upa.calendar.model.service;
-
-public interface CalendarService {
-
-}
+/*
+ * package edu.fin.upa.calendar.model.service;
+ * 
+ * import java.util.List;
+ * 
+ * import edu.fin.upa.calendar.model.vo.CalendarTest;
+ * 
+ * public interface CalendarService {
+ * 
+ * List<CalendarTest> selectCalTest(String startDate);
+ * 
+ * }
+ */
--- wooper/src/main/java/edu/fin/upa/calendar/model/service/CalendarServiceImpl.java
+++ wooper/src/main/java/edu/fin/upa/calendar/model/service/CalendarServiceImpl.java
@@ -1,8 +1,23 @@
-package edu.fin.upa.calendar.model.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CalendarServiceImpl implements CalendarService{
-
-}
+//package edu.fin.upa.calendar.model.service;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import edu.fin.upa.calendar.model.dao.CalendarDAO;
+//import edu.fin.upa.calendar.model.vo.CalendarTest;
+//
+//
+//@Service
+//public class CalendarServiceImpl implements CalendarService{
+//
+//	@Autowired
+//	private CalendarDAO dao;
+//	
+//	@Override
+//	public List<CalendarTest> selectCalTest(String startDate) {
+//		return dao.selectCalTest(startDate);
+//	}
+//	
+//}
